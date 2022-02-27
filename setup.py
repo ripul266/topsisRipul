9df1b6d65f7793b4f1b9a9acf436f086c0cce046
ripul266 <51872222+ripul266@users.noreply.github.com>
2022-02-27 16:21:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'topsisRipul',         # How you named your package folder (MyLib)
   packages = ['topsisRipul'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python Package to rank Features in a ML model',   # Give a short description about your library
   author = 'Ripul Ahuja',                   # Type in your name
